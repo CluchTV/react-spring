992f1247465ab1bc560dacac8783d2fcd4568bf9
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-04-23 11:05:26 +0100

    chore: rework build pipeline (again)

--- tsup.config.base.ts
+++ tsup.config.base.ts
@@ -1,5 +1,93 @@
 import type { Options } from 'tsup'
 import fs from 'fs'
+import path from 'path'
+
+interface BuildOptions {
+  format: 'cjs' | 'umd' | 'esm'
+  name:
+    | 'development'
+    | 'production.min'
+    | 'legacy-esm'
+    | 'modern'
+    | 'modern.development'
+    | 'modern.production.min'
+    | 'umd'
+    | 'umd.min'
+  minify: boolean
+  env: 'development' | 'production' | ''
+  target?:
+    | 'es2017'
+    | 'es2018'
+    | 'es2019'
+    | 'es2020'
+    | 'es2021'
+    | 'es2022'
+    | 'esnext'
+  dts?: boolean
+}
+
+const buildTargets: BuildOptions[] = [
+  {
+    format: 'cjs',
+    name: 'development',
+    target: 'esnext',
+    minify: false,
+    env: 'development',
+  },
+  {
+    format: 'cjs',
+    name: 'production.min',
+    target: 'esnext',
+    minify: true,
+    env: 'production',
+  },
+  // ESM, embedded `process`: modern Webpack dev
+  {
+    format: 'esm',
+    name: 'modern',
+    target: 'esnext',
+    minify: false,
+    env: '',
+    dts: true,
+  },
+  // ESM, embedded `process`: fallback for Webpack 4,
+  // which doesn't support `exports` field or optional chaining
+  {
+    format: 'esm',
+    name: 'legacy-esm',
+    target: 'esnext',
+    minify: false,
+    env: '',
+  },
+  // ESM, pre-compiled "dev": browser development
+  {
+    format: 'esm',
+    name: 'modern.development',
+    target: 'esnext',
+    minify: false,
+    env: 'development',
+  },
+  // ESM, pre-compiled "prod": browser prod
+  {
+    format: 'esm',
+    name: 'modern.production.min',
+    target: 'esnext',
+    minify: true,
+    env: 'production',
+  },
+]
+
+function writeCommonJSEntry(folder: string, prefix: string) {
+  fs.writeFileSync(
+    path.join(folder, 'index.js'),
+    `'use strict'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./${prefix}.production.min.cjs')
+} else {
+  module.exports = require('./${prefix}.development.cjs')
+}`
+  )
+}
 
 interface ConfigOptions {
   name: string
@@ -7,101 +95,71 @@ interface ConfigOptions {
 }
 
 export const defaultConfig = (
-  { name, entry }: ConfigOptions,
+  { name: prefix, entry }: ConfigOptions,
   options: Options
 ): Options[] => {
-  const commonOptions: Partial<Options> = {
-    entry: {
-      [name]: entry,
-    },
-    sourcemap: true,
-    ...options,
-  }
+  const artifactOptions: Options[] = buildTargets.map(
+    ({ format, minify, env, name, target, dts }) => {
+      const outputFilename = `${prefix}.${name}`
 
-  const productionOptions = {
-    minify: true,
-    define: {
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    },
-  }
+      const folderSegments = ['dist']
+      if (format === 'cjs') {
+        folderSegments.push('cjs')
+      }
 
-  return (
-    [
-      // ESM, standard bundler dev, embedded `process` references
-      {
-        ...commonOptions,
-        format: ['esm'],
-        dts: true,
+      const outputFolder = path.join(...folderSegments)
+
+      const extension =
+        name === 'legacy-esm' ? '.js' : format === 'esm' ? '.mjs' : '.cjs'
+
+      const defineValues: Record<string, string> = {}
+
+      if (env) {
+        Object.assign(defineValues, {
+          'process.env.NODE_ENV': JSON.stringify(env),
+        })
+      }
+
+      return {
+        entry: {
+          [outputFilename]: entry,
+        },
+        dts,
+        format,
+        outDir: outputFolder,
+        target,
+        outExtension: () => ({ js: extension }),
+        minify,
         sourcemap: true,
-      },
-      // ESM, Webpack 4 support. Target ES2018 syntax to compile away optional chaining and spreads
-      {
-        ...commonOptions,
-        entry: {
-          [`${name}.legacy-esm`]: entry,
+        clean: !options.watch,
+        external: [
+          'react',
+          'react-dom',
+          'react-native',
+          '@react-three/fiber',
+          'three',
+          'react-konva',
+          'konva',
+          'react-zdog',
+          'zdog',
+        ],
+        esbuildOptions(options) {
+          // Needed to prevent auto-replacing of process.env.NODE_ENV in all builds
+          options.platform = 'neutral'
+          // Needed to return to normal lookup behavior when platform: 'neutral'
+          options.mainFields = ['browser', 'module', 'main']
+          options.conditions = ['browser']
         },
-        // ESBuild outputs `'.mjs'` by default for the 'esm' format. Force '.js'
-        outExtension: () => ({ js: '.js' }),
-        target: 'es2017',
-        format: ['esm'],
-        sourcemap: true,
-      },
-      // ESM for use in browsers. Minified, with `process` compiled away
-      {
-        ...commonOptions,
-        ...productionOptions,
-        entry: {
-          [`${name}.production`]: entry,
+
+        define: defineValues,
+        async onSuccess() {
+          if (format === 'cjs' && name === 'production.min') {
+            writeCommonJSEntry(outputFolder, prefix)
+          }
         },
-        format: ['esm'],
-        outExtension: () => ({ js: '.mjs' }),
-      },
-      // CJS development
-      {
-        ...commonOptions,
-        entry: {
-          [`${name}.cjs.development`]: entry,
-        },
-        format: 'cjs',
-        outDir: './dist/',
-      },
-      // CJS production
-      {
-        ...commonOptions,
-        ...productionOptions,
-        entry: {
-          [`${name}.cjs.production.min`]: entry,
-        },
-        format: 'cjs',
-        outDir: './dist/',
-        onSuccess: async () => {
-          // Write the CJS index file
-          fs.writeFileSync(
-            'dist/index.js',
-            `
-'use strict'
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./${name}.cjs.production.min.js')
-} else {
-  module.exports = require('./${name}.cjs.development.js')
-}`
-          )
-        },
-      },
-    ] as Options[]
-  ).map(config => ({
-    ...config,
-    clean: !options.watch,
-    external: [
-      'react',
-      'react-dom',
-      'react-native',
-      '@react-three/fiber',
-      'three',
-      'react-konva',
-      'konva',
-      'react-zdog',
-      'zdog',
-    ],
-  }))
+      }
+    }
+  )
+
+  return artifactOptions
 }
